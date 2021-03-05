7ca5d27f756b49c6d1e18e76dd7ce6f277b78d1b
Elton Lac <lacelton@yahoo.com>
2021-03-04 19:39:34 -0800

    Updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
+const PORT = process.env.PORT || 8080;
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
