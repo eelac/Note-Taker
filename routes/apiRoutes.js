09c89c7c7d3f686927fee9b0d3935056df356b00
Elton Lac <lacelton@yahoo.com>
2021-03-04 21:10:38 -0800

    Update

---
+++ routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+const uuid = require('uuid');
+
+const dbPath = "../db/db.json";
+
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, dbPath));
+})
+
+
+router.post('/notes', (req, res) => {
+  const note = JSON.parse(fs.readFileSync(dbPath));
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  note.push(newNote);
+  fs.writeFileSync(dbPath, JSON.stringify(note))
+  res.json(note);
+})
+
+
+router.delete("/notes/:id", (req, res) => {
+  const note = JSON.parse(fs.readFileSync(dbPath));
+  const deleteNote = note.filter((delNote) => delNote.id !== req.params.id);
+  fs.writeFileSync(dbPath, JSON.stringify(deleteNote));
+  res.json(deleteNote);
+})
+
+
+module.exports = router;
